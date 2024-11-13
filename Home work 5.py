f88bad81d34f640417007be0da64f3405ea29036
Nikolay5-97 <nikolajkusnarev626@gmail.com>
2024-11-13 17:38:40 +0700

    Add files via upload

---
+++ Home work 5.py
@@ -0,0 +1,13 @@
+my_list = ["orange", 'apple', 'coconut', 'banana', 'fruit', 'kiwi', 'greenapple']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'mango'
+print(my_list)
+
+my_dict = {'name': 'имя', 'city': 'город', 'car': 'машина', 'carrot': 'морковь'}
+print(my_dict)
+print(my_dict['name'])
+my_dict['family'] = 'семья'
+print(my_dict)
